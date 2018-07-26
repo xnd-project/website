f0102f9ce6a05f52cc0106c2d5b19bbb59ea1a45
Anderson Banihirwe <axbanihirwe@ualr.edu>
2018-07-25 23:56:24 -0400

    remove unncessary  css and  js files

--- js/tabs.js
+++
@@ -1,8 +0,0 @@
-function openTab(group, name) {
-    var i;
-    var x = document.getElementsByClassName(group);
-    for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";
-    }
-    document.getElementById(name).style.display = "block";
-}
